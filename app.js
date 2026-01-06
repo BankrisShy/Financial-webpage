ef3c717aeab2cfb8fc56f4cbafaef8bfcd6cf8ad
Patrick Agbanu <agbanuyawabella@gmail.com>
2026-01-06 13:39:14 +0000

    render requirements

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:5000";
+const API_URL = "https://financial-webpage.onrender.com";
 
 const path = window.location.pathname;
 const isAdminPage = path.endsWith("admin.html");
